7ccbf9f9a44e30037e0c920dfe08d6b349d55afc
Gerald Soriano <sorianog@msoe.edu>
2017-02-04 13:20:34 -0600

    Going through Demo video of Introduction to R section of Data Science with R

--- language-basics.R
+++ language-basics.R
@@ -7,3 +7,53 @@ y = "Hello World!"
 
 # Implicit printing
 print(x)
+x
+
+# Creating variables
+l <- TRUE
+i <- 123L
+n <- 1.23
+c <- "ABC 123"
+d <- as.Date("2001-02-03")
+
+# Displaying variables
+l
+i
+n
+c
+d
+
+# Creating a function
+f <- function(x) { x + 1 }
+
+# Invoking a function
+f(2)
+
+# Creating a vector
+v <- c(1, 2, 3);
+v 
+
+# Creating a sequence
+m <- matrix(
+  data = 1:6,
+  nrow = 2,
+  ncol = 3
+)
+m
+
+# Creating an array
+a <- array(
+  data = 1:8,
+  dim = c(2, 2, 2)
+)
+a
+
+# Creating a list 
+l <- list(TRUE, 123L, 2.34, "abc")
+l
+
+# Creating a factor
+categories <- c("Male", "Female", "Male", "Male", 
+factor <- factor(categories)
+factor
+levels(factor)
