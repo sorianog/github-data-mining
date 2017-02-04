570d84f5f45e5f064dc17c6f3df131071c32dc47
Gerald Soriano <sorianog@msoe.edu>
2017-02-04 14:21:47 -0600

    Finished basic demo module for Data Science with R

--- language-basics.R
+++ language-basics.R
@@ -53,7 +53,54 @@ l <- list(TRUE, 123L, 2.34, "abc")
 l
 
 # Creating a factor
-categories <- c("Male", "Female", "Male", "Male", 
+categories <- c("Male", "Female", "Male", "Male", "Female")
 factor <- factor(categories)
 factor
 levels(factor)
+unclass(factor)
+
+# Creating a data frame
+df <- data.frame(
+    Name = c("Cat", "Dog", "Cow", "Pig"),
+    HowMany = c(5, 10, 15, 20),
+    IsPet = c(TRUE, TRUE, FALSE, FALSE)
+)
+df
+
+# Indexing data frames by row and column
+df[1, 2]
+
+# Indexing data frame by row
+df[1, ]
+
+# Indexing data frames by column
+df[ , 2]
+df[["HowMany"]]
+df$HowMany
+
+# Subsetting data frames
+df[c(2, 4), ]
+df[2:4, ]
+df[c(TRUE, FALSE, TRUE, FALSE), ]
+df[df$IsPet == TRUE]
+df[df$HowMany > 10, ]
+df[df$Name %in% c("Cat", "Cow"), ]
+
+# R is a vectorized language
+1 + 2
+c(1, 2, 3) + c(2, 4, 6)
+
+# Name vs. ordered arguments 
+m <- matrix(data = 1:6, nrow = 2, ncol = 3)
+n <- matrix(1:6, 2, 3)
+m == n
+identical(m, n)
+
+# Install packages
+install.packages("dplyr")
+
+# Loading packages
+library("dplyr")
+
+# Viewing help
+?data.frame
