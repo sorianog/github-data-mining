80ea296f82a565f0a3cd3cb22825ef22765c7404
Gerald Soriano <sorianog@msoe.edu>
2017-02-08 23:45:23 -0600

    Finished module for using R functions to analyze a Cars data set.

--- working-with-data.R
+++ working-with-data.R
@@ -13,3 +13,79 @@ cars <- read.table(
 
 # Peek at the data
 head(cars)
+
+# Load the dplyr library
+library(dplyr)
+
+# Select a subset of columns
+temp <- select(
+  .data = cars,
+  Transmission,
+  Cylinders,
+  Fuel.Economy
+)
+
+# Inspect the results
+head(temp)
+
+# Filter a subset of rows
+temp <- filter(
+  .data = temp,
+  Transmission == "Automatic"
+)
+
+# Inspect the results
+head(temp)
+
+# Compute a new column
+temp <- mutate(
+  .data = temp,
+  Consumption = Fuel.Economy * 0.425
+)
+
+# Inspect the results
+head(temp)
+
+# Group by a column
+temp <- group_by(
+  .data = temp,
+  Cylinders
+)
+
+# Inspect the results
+head(temp)
+
+# Aggregate based on groups
+temp <- summarize(
+  .data = temp,
+  Avg.Consumption = mean(Consumption)
+)
+
+# Inspect the results 
+head(temp)
+
+# Convert to data frame
+efficiency <- as.data.frame(temp)
+
+# Inspect the results
+print(efficiency)
+
+# Chain methods together
+efficiency <- cars %>%
+  select(Fuel.Economy, Cylinders, Transmission) %>%
+  filter(Transmission == "Automatic") %>%
+  mutate(Consumption = Fuel.Economy * 0.425) %>%
+  group_by(Cylinders) %>%
+  summarize(Avg.Consumption = mean(Consumption)) %>%
+  arrange(desc(Avg.Consumption)) %>%
+  as.data.frame()
+
+# Inspect the results
+print(efficiency)
+
+# Save the results to a CSV file
+write.csv(
+  x = efficiency,
+  file = "FuelEfficiencyTutorial.csv",
+  row.names = FALSE
+)
